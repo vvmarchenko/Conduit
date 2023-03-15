cb36f2aef0b3c291fe13b008502462edec5e2494
Vladyslav Marchenko <111642735+vvmarchenko@users.noreply.github.com>
2023-03-15 20:35:51 +0200

    Delete conduit_backend/realWorld-server directory

--- conduit_backend/realWorld-server/realWorld-server/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = [
-    'http://localhost',
-    'http://localhost:3000',
-    'http://localhost:4200',
-    'https://winterrrrrff.github.io',
-    'https://angular-realworld-example-app-neon.vercel.app'
-];
-
-module.exports = allowedOrigins;
